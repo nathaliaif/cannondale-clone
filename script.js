fe57dfa985c54b376ff708c35b3602a26c678999
Nathalia Irokawa <nathalia.iro.fernandes@gmail.com>
2024-07-19 17:39:04 -0300

    Adicionado funcionalidade na navbar

--- script.js
+++ script.js
@@ -1,17 +1,91 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+// ---- Navbar changes background when Scrolled ----
+const navLinks = document.querySelectorAll('.navbar__links a');
+const navSearch = document.querySelector('.navbar__search a');
+const navLogo = document.querySelector('.navbar__logo');
+const mainNavbar = document.getElementById("navbar");
 
-// Get the navbar
-    var navbar = document.getElementById("main-navbar");
+function changeLinkColor(color) {
+  navSearch.style.color = color;
 
-// Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
+  navLinks.forEach(element => {
+    element.style.color = color;
+  })
+}
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
+function navScroll(){
+  mainNavbar.classList.add('navbarScrolled');
+  navLogo.setAttribute('src', 'img/cannondale-logo-black.svg');
+  changeLinkColor('var(--preto)');
+}
+
+function navTop(){
+  mainNavbar.classList.remove('navbarScrolled');
+  navLogo.setAttribute('src', 'img/cannondale-logo-white.svg');
+  changeLinkColor('var(--branco)');
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100){
+    navScroll();
+  } else {
+    navTop();
+  }
+})
+
+
+// --- Show navbar items on hover ----
+const navbarItems = document.querySelectorAll('.navbar__links-container li');
+const navbarItemsContainer = document.querySelector('.navbar__links-container');
+const navbarSublinksContainer = document.querySelector('.navbar__sublinks-container');
+let uniqueClass = '';
+let sublinks = '';
+
+
+function showSublinksContainer(){
+  navbarSublinksContainer.style.display = 'block';
+  navScroll();
+}
+
+function hideSublinks(){
+  navbarSublinksContainer.style.display = 'none';
+
+  if (window.scrollY === 0){
+    navTop();
+  }
+}
+
+navbarItems.forEach(item => {
+  item.addEventListener('mouseover', (event => {
+    uniqueClass = Array.from(item.classList).find(className => className !== 'navbar__links')
+    sublinks = document.querySelector(`.sublinks-${uniqueClass}`);
+
+    if(sublinks){
+      sublinks.classList.add('active');
     }
+  }))
+
+  item.addEventListener('mouseout', () => {
+    uniqueClass = Array.from(item.classList).find(className => className !== 'navbar__links')
+    sublinks = document.querySelector(`.sublinks-${uniqueClass}`);
+
+    sublinks.classList.remove('active');
+  })
+
+})
+
+navbarItemsContainer.addEventListener('mouseover', showSublinksContainer);
+navbarSublinksContainer.addEventListener('mouseover', showSublinksContainer);
+
+navbarItemsContainer.addEventListener('mouseout', (event) => {
+  if (!navbarSublinksContainer.contains(event.relatedTarget)){
+    hideSublinks();
+  }
+})
+
+navbarSublinksContainer.addEventListener('mouseout', (event) => {
+  if (!navbarItemsContainer.contains(event.relatedTarget)){
+    hideSublinks();
+  }
+})
+
+
